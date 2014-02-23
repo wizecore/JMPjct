c6923d2d93946ac2eea4c6b44618dcc40190933a
Rob Smith <kormoc@gmail.com>
2014-02-23 15:17:38 -0800

    More unit tests

--- src/com/github/mpjct/jmpjct/mysql/proto/ProtoTest.java
+++ src/com/github/mpjct/jmpjct/mysql/proto/ProtoTest.java
@@ -6,7 +6,7 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ProtoTest {
-    
+
     @Test
     public void test_all_byte_values() {
         byte[] packet = new byte[256];
@@ -15,24 +15,24 @@ public class ProtoTest {
             packet[offset] = (byte)b;
             offset = offset + 1;
         }
-        
+
         packet[offset] = (byte)0x7F;
         offset = offset + 1;
-        
+
         for (byte b = (byte)0x80; b <= (byte)0xFF; b++) {
             packet[offset] = (byte)b;
             offset = offset + 1;
         }
-        
+
         Proto proto = new Proto(packet);
-        
+
         ArrayList<byte[]> payload = new ArrayList<byte[]>();
-        
+
         payload.add( Proto.build_fixed_str(packet.length, proto.get_fixed_str(packet.length, true), true));
-        
+
         assertArrayEquals(packet, Proto.arraylist_to_array(payload));
     }
-    
+
     @Test
     public void test1() {
         byte[] expected = new byte[] {
@@ -40,7 +40,7 @@ public class ProtoTest {
         };
         assertArrayEquals(expected, Proto.build_fixed_int(1, 0));
     }
-    
+
     @Test
     public void test2() {
         byte[] expected = new byte[] {
@@ -48,7 +48,7 @@ public class ProtoTest {
         };
         assertArrayEquals(expected, Proto.build_fixed_int(1, 255));
     }
-    
+
     @Test
     public void test3() {
         byte[] expected = new byte[] {
@@ -56,7 +56,7 @@ public class ProtoTest {
         };
         assertArrayEquals(expected, Proto.build_fixed_int(2, 0));
     }
-    
+
     @Test
     public void test4() {
         byte[] expected = new byte[] {
@@ -64,7 +64,7 @@ public class ProtoTest {
         };
         assertArrayEquals(expected, Proto.build_fixed_int(2, 0xFFFF));
     }
-    
+
     @Test
     public void test5() {
         byte[] expected = new byte[] {
@@ -72,7 +72,7 @@ public class ProtoTest {
         };
         assertArrayEquals(expected, Proto.build_fixed_int(3, 0));
     }
-    
+
     @Test
     public void test6() {
         byte[] expected = new byte[] {
@@ -80,7 +80,7 @@ public class ProtoTest {
         };
         assertArrayEquals(expected, Proto.build_fixed_int(4, 0));
     }
-    
+
     @Test
     public void test7() {
         byte[] expected = new byte[] {
@@ -88,7 +88,7 @@ public class ProtoTest {
         };
         assertArrayEquals(expected, Proto.build_fixed_int(8, 0));
     }
-    
+
     @Test
     public void test8() {
         byte[] expected = new byte[] {
@@ -96,7 +96,7 @@ public class ProtoTest {
         };
         assertArrayEquals(expected, Proto.build_fixed_int(8, 255));
     }
-    
+
     @Test
     public void test9() {
         byte[] expected = new byte[] {
@@ -104,7 +104,7 @@ public class ProtoTest {
         };
         assertArrayEquals(expected, Proto.build_lenenc_int(0));
     }
-    
+
     @Test
     public void test10() {
         byte[] expected = new byte[] {
@@ -112,7 +112,7 @@ public class ProtoTest {
         };
         assertArrayEquals(expected, Proto.build_lenenc_int(251));
     }
-    
+
     @Test
     public void test11() {
         byte[] expected = new byte[] {
@@ -120,7 +120,7 @@ public class ProtoTest {
         };
         assertArrayEquals(expected, Proto.build_lenenc_int(252));
     }
-    
+
     @Test
     public void test12() {
         byte[] expected = new byte[] {
@@ -128,7 +128,7 @@ public class ProtoTest {
         };
         assertArrayEquals(expected, Proto.build_lenenc_int(65536));
     }
-    
+
     @Test
     public void test13() {
         byte[] expected = new byte[] {
@@ -137,7 +137,7 @@ public class ProtoTest {
         };
         assertArrayEquals(expected, Proto.build_lenenc_int(16777216));
     }
-    
+
     @Test
     public void test14() {
         byte[] expected = new byte[] {
@@ -146,7 +146,7 @@ public class ProtoTest {
         };
         assertArrayEquals(expected, Proto.build_lenenc_int(33554432));
     }
-    
+
     @Test
     public void test15() {
         byte[] expected = new byte[] {
@@ -237,7 +237,7 @@ public class ProtoTest {
 
         assertArrayEquals(expected, Proto.build_lenenc_str(str));
     }
-    
+
     @Test
     public void test18() {
         byte[] expected = new byte[] {
@@ -246,7 +246,7 @@ public class ProtoTest {
         String str = new String("");
         assertArrayEquals(expected, Proto.build_null_str(str));
     }
-    
+
     @Test
     public void test19() {
         byte[] expected = new byte[] {
@@ -255,16 +255,16 @@ public class ProtoTest {
         String str = new String("abc");
         assertArrayEquals(expected, Proto.build_null_str(str));
     }
-    
+
     @Test
     public void test20() {
         byte[] expected = new byte[] {
-            (byte)0x00, (byte)0x00, (byte)0x00, 
+            (byte)0x00, (byte)0x00, (byte)0x00,
         };
         String str = new String("");
         assertArrayEquals(expected, Proto.build_fixed_str(3, str));
     }
-    
+
     @Test
     public void test21() {
         byte[] expected = new byte[] {
@@ -273,7 +273,7 @@ public class ProtoTest {
         String str = new String("abc");
         assertArrayEquals(expected, Proto.build_fixed_str(3, str));
     }
-    
+
     @Test
     public void test22() {
         byte[] expected = new byte[] {
@@ -282,7 +282,7 @@ public class ProtoTest {
         String str = new String("abc");
         assertArrayEquals(expected, Proto.build_fixed_str(2, str));
     }
-    
+
     @Test
     public void test23() {
         byte[] expected = new byte[] {
@@ -291,4 +291,62 @@ public class ProtoTest {
         String str = new String("ab");
         assertArrayEquals(expected, Proto.build_eop_str(str));
     }
+
+    @Test
+    public void test24() {
+        byte[] expected = new byte[] {
+            (byte)0x00,
+        };
+        assertArrayEquals(expected, Proto.build_filler(1));
+    }
+
+    @Test
+    public void test25() {
+        byte[] expected = new byte[] {
+            (byte)0x00, (byte)0x00,
+        };
+        assertArrayEquals(expected, Proto.build_filler(2));
+    }
+
+    @Test
+    public void test26() {
+        byte[] expected = new byte[] {
+            (byte)0x1C, (byte)0x1C,
+        };
+        assertArrayEquals(expected, Proto.build_filler(2, (byte) 0x1C));
+    }
+
+    @Test
+    public void test27() {
+        byte[] expected = new byte[] {
+            (byte)0xFF, (byte)0xFF,
+        };
+        assertArrayEquals(expected, Proto.build_filler(2, (byte) 0xFF));
+    }
+
+    @Test
+    public void test28() {
+        byte[] expected = new byte[] {
+            (byte)0x00,
+        };
+        assertArrayEquals(expected, Proto.build_byte((byte)0x00));
+    }
+    
+    @Test
+    public void test29() {
+        byte[] expected = new byte[] {
+            (byte)0x01,
+        };
+        assertArrayEquals(expected, Proto.build_byte((byte)0x01));
+    }
+    
+    @Test
+    public void test30() {
+        byte[] expected = new byte[] {
+            (byte)0xFF,
+        };
+        assertArrayEquals(expected, Proto.build_byte((byte)0xFF));
+    }
+    
+    
 }
