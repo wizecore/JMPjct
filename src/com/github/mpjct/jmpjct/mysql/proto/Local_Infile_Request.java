c8be6c4bc5b5a3fa4e02ad28220ac51083051d94
Rob Smith <kormoc@gmail.com>
2014-02-23 22:09:55 -0800

    Local_Infile_RequestTest.java Local_Infile_Request.java

--- src/com/github/mpjct/jmpjct/mysql/proto/Local_Infile_Request.java
+++ src/com/github/mpjct/jmpjct/mysql/proto/Local_Infile_Request.java
@@ -9,6 +9,7 @@ public class Local_Infile_Request extends Packet {
     public ArrayList<byte[]> getPayload() {
         ArrayList<byte[]> payload = new ArrayList<byte[]>();
         
+        payload.add(Proto.build_byte(Flags.LOCAL_INFILE));
         payload.add( Proto.build_eop_str(this.filename));
         
         return payload;
@@ -19,6 +20,7 @@ public class Local_Infile_Request extends Packet {
         Proto proto = new Proto(packet, 3);
         
         obj.sequenceId = proto.get_fixed_int(1);
+        proto.get_filler(1);
         obj.filename = proto.get_eop_str();
 
         return obj;
